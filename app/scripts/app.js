c9a02c789f7b599d043f1a1471268b990b746c86
TeresaHita <teresahitamedia@gmail.com>
2016-06-10 19:17:53 +0200

    Estructura inicial del proyecto

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appLibrosPecApp
+ * @description
+ * # appLibrosPecApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appLibrosPecApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appLibrosPecApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appLibrosPecApp
+ */
+angular.module('appLibrosPecApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
